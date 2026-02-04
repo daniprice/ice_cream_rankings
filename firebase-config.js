6628f0dd7583d444643f2e1d66cd15ee139b216b
dani price <152434162+daniprice@users.noreply.github.com>
2026-02-04 11:19:02 -0500

    Add Firebase configuration file

---
+++ firebase-config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAvAEsTD5DG1L8wbyvv4i_pE8gdbBOi2Yo",
+  authDomain: "ice-cream-rankings-90d57.firebaseapp.com",
+  projectId: "ice-cream-rankings-90d57",
+  storageBucket: "ice-cream-rankings-90d57.firebasestorage.app",
+  messagingSenderId: "1090540037817",
+  appId: "1:1090540037817:web:4c44918c64ec3d55ecdd68"
+};
+
+export default firebaseConfig;
